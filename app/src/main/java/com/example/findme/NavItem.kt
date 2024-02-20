90a64426415ca012d84d974a68890306b2af4db5
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-02-21 05:04:20 +0600

    bruh

--- app/src/main/java/com/example/findme/NavItem.kt
+++ app/src/main/java/com/example/findme/NavItem.kt
@@ -1,1 +1,8 @@
-package org.jetbrains.
+package com.example.findme
+
+sealed class NavItem(val route: String, val icon: Int) {
+    object Search : NavItem("search", R.drawable.search)
+    object Cam : NavItem("cam", R.drawable.cam)
+    object Chat : NavItem("chat", R.drawable.chat)
+    object Profile : NavItem("profile", R.drawable.profile)
+}
