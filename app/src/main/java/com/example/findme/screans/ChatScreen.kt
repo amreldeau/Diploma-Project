e27d5fd8c658effdd938dcbcc86f7156fae11058
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-02-21 21:09:41 +0600

    add test screens
    add test screens

---
+++ app/src/main/java/com/example/findme/screans/ChatScreen.kt
@@ -0,0 +1,31 @@
+package com.example.findme.screans
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.findme.ui.theme.AlegreyaFontFamily
+
+@Composable
+fun ChatScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        Text(
+            text = "Chat Screen",
+            fontSize = 30.sp,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center,
+            fontFamily = AlegreyaFontFamily
+        )
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 640)
+@Composable
+fun ChatScreenPreview() {
+    ChatScreen()
+}
