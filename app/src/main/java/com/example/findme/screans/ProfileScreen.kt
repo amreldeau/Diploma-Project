796a2be3356c4a9ce196fbad0f0375811658f1cb
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-02-22 22:40:04 +0600

    start

--- app/src/main/java/com/example/findme/screans/ProfileScreen.kt
+++ app/src/main/java/com/example/findme/screans/ProfileScreen.kt
@@ -1,25 +1,95 @@
 package com.example.findme.screans
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.findme.ui.theme.AlegreyaFontFamily
+import com.example.findme.R
+import com.example.findme.ui.theme.Lato
 
 @Composable
 fun ProfileScreen() {
-    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-        Text(
-            text = "Profile Screen",
-            fontSize = 30.sp,
-            fontWeight = FontWeight.Bold,
-            fontFamily = AlegreyaFontFamily
-        )
+
+    Box(modifier =  Modifier.fillMaxSize()){
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally // Align items to the center
+        ) {
+            Spacer(modifier = Modifier.size(20.dp))
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = null,
+                modifier = Modifier
+                    .size(200.dp)
+                    .clip(CircleShape)
+                    .border(
+                        shape = CircleShape,
+                        border = BorderStroke(2.dp, Color.White)
+                    )
+            )
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(
+                text = "USER NAME",
+                color = Color.Black,
+                style = androidx.compose.ui.text.TextStyle(
+                    fontSize = 25.sp,
+                    textAlign = TextAlign.Center, // Center align text
+                    fontFamily = Lato,
+                    fontWeight = FontWeight.Bold
+                )
+            )
+
+            Spacer(modifier = Modifier.size(20.dp))
+            Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(elevation = 5.dp, shape = RoundedCornerShape(10.dp)), // Add this line
+            colors = ButtonDefaults.buttonColors(Color(0xFF59C9A5)) // Correct parameter name
+            ) {
+            Text(text = "SETTINGS")
+            }
+
+            Spacer(modifier = Modifier.size(20.dp))
+            Button(
+            onClick = { /*TODO*/ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .shadow(elevation = 5.dp, shape = RoundedCornerShape(10.dp)), // Add this line
+            colors = ButtonDefaults.buttonColors(Color(0xFF59C9A5)) // Correct parameter name
+            ) {
+                Text(
+                    text = "EDIT PROFILE"
+
+                )
+            }
+        }
     }
 }
 
--- app/src/main/java/com/example/findme/ui/theme/Type.kt
+++ app/src/main/java/com/example/findme/ui/theme/Type.kt
@@ -4,6 +4,7 @@ import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import com.example.findme.R
@@ -44,4 +45,20 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val Lato = FontFamily(
+    Font(R.font.lato_bold, FontWeight.Bold),
+    Font(R.font.lato_regular, FontWeight.Normal),
+    Font(R.font.lato_light, FontWeight.Light),
+
+    Font(R.font.lato_thin, FontWeight.Thin),
+    Font(R.font.lato_thin_italic, FontWeight.Thin, FontStyle.Italic),
+
+    Font(R.font.lato_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.lato_bold_italic, FontWeight.Bold, FontStyle.Italic),
+    Font(R.font.lato_light_italic, FontWeight.Light, FontStyle.Italic),
+
+    Font(R.font.lato_black, FontWeight.Black),
+    Font(R.font.lato_black_italic, FontWeight.Black, FontStyle.Italic)
 )
