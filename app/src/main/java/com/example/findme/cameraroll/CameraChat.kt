3159c1fdd10d66921f397147c2291eb2a082aa1c
Abzhik <52734118+Abjik@users.noreply.github.com>
2024-03-20 13:42:19 +0500

    jhg

--- app/src/main/java/com/example/findme/cameraroll/CameraChat.kt
+++ app/src/main/java/com/example/findme/cameraroll/CameraChat.kt
@@ -1,55 +1,55 @@
 package com.example.findme.cameraroll
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CameraChat(){
-    val (userName, setUserName) = remember { mutableStateOf("") }
-    val (message, setMessage) = remember { mutableStateOf("") }
+    val (message, onMessageChange) = remember { mutableStateOf(TextFieldValue()) }
 
     Column(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(20.dp),
-        horizontalAlignment = Alignment.CenterHorizontally // Align items to the center
+        modifier = Modifier.fillMaxSize()
     ) {
-
-        OutlinedTextField(
-            value = userName,
-            onValueChange = setUserName,
-            label = { Text("User name") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        OutlinedTextField(
-            value = message,
-            onValueChange = setMessage,
-            label = { Text("Type your message") },
-            modifier = Modifier.fillMaxWidth()
-        )
-
-        Button(
-            onClick = { /* Do something with the user name and message */ },
-            modifier = Modifier.padding(top = 16.dp).
-            fillMaxWidth().
-            height(50.dp)
+        Box(
+            modifier = Modifier.weight(1f)
         ) {
-            Text("SEND")
+            // Список сообщений
+        }
+        Divider()
+        Row(
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            TextField(
+                value = message,
+                onValueChange = onMessageChange,
+                modifier = Modifier.weight(1f)
+            )
+            Button(
+                onClick = { /* Отправить сообщение */ },
+                modifier = Modifier.padding(start = 8.dp)
+            ) {
+                Text("Send")
+            }
         }
     }
 }
