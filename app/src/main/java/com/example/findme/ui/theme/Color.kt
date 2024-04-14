ebd0dd142de58d3522f87d0d9b740d3b044fc22a
Yeldos <eldos.amire@gmail.com>
2024-04-15 04:42:46 +0500

    Implemented chats: data source is manually input for now

--- app/src/main/java/com/example/findme/ui/theme/Color.kt
+++ app/src/main/java/com/example/findme/ui/theme/Color.kt
@@ -5,3 +5,4 @@ import androidx.compose.ui.graphics.Color
 val Green = Color(0xFF008068)
 val LightGreen = Color(0xFF007F69)
 val LightGray = Color(0xFF131920)
+val HighlightGreen = Color(0xFF25D366)
