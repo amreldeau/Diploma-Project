bc0f2633d8a1b8db6187e619e93494da76aa4971
Yeldos <eldos.amire@gmail.com>
2024-04-30 12:44:33 +0500

    Managed to integrate chat, but client is not being initialized

--- app/src/main/java/com/example/findme/screens/VideoChatScreen.kt
+++ app/src/main/java/com/example/findme/screens/VideoChatScreen.kt
@@ -22,9 +22,9 @@ fun VideoChatScreen() {
     // Define your userToken, userId, and callId
     // val user = userRepository.getUser(userId)
     // val token = userRepository.getToken(userId)
-    val userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiQWRtaXJhbF9BY2tiYXIiLCJpc3MiOiJodHRwczovL3Byb250by5nZXRzdHJlYW0uaW8iLCJzdWIiOiJ1c2VyL0FkbWlyYWxfQWNrYmFyIiwiaWF0IjoxNzE0MDczMDI3LCJleHAiOjE3MTQ2Nzc4MzJ9.lAaZpwl9dlnCz92Nf16aW_9O9ZN8sCmjPv4E_DlCl9s"
-    val userId = "Admiral_Ackbar"
-    val callId = "WnTJkiHtEIUx"
+    val userToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiR2VuZXJhbF9Hcmlldm91cyIsImlzcyI6Imh0dHBzOi8vcHJvbnRvLmdldHN0cmVhbS5pbyIsInN1YiI6InVzZXIvR2VuZXJhbF9Hcmlldm91cyIsImlhdCI6MTcxNDQzMjE2NSwiZXhwIjoxNzE1MDM2OTcwfQ.PrmO2xXU--XWwCIw8-sa4SdDaSsr8oK5QmAmb1-zEbE"
+    val userId = "General_Grievous"
+    val callId = "9fLrdT6PbXrU"
 
     // Use LaunchedEffect to call joinCall when the Composable is first composed
     LaunchedEffect(Unit) {
