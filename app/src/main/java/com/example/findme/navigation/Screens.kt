ba3d347d098a389617877debde1dfc0055695917
Yeldos <eldos.amire@gmail.com>
2024-05-04 04:27:39 +0500

    Added view model & UI for Chat Messages. Implemented logic when chat list item (navigate to ChatMessagesScreen from ChatScreen

--- app/src/main/java/com/example/findme/navigation/Screens.kt
+++ app/src/main/java/com/example/findme/navigation/Screens.kt
@@ -1,5 +1,5 @@
 package com.example.findme.navigation
 
 enum class Screens {
-    HomeScreen, VideoChatScreen, ChatScreen, ProfileScreen, SettingsScreen, ChangeProfile1, ChangeProfile2, ChangeProfile3
+    HomeScreen, VideoChatScreen, ChatScreen, ChatMessagesScreen, ProfileScreen, SettingsScreen, ChangeProfile1, ChangeProfile2, ChangeProfile3
 }
--- app/src/main/java/com/example/findme/screens/ChatScreen.kt
+++ app/src/main/java/com/example/findme/screens/ChatScreen.kt
@@ -8,7 +8,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import com.example.findme.R
+import com.example.findme.navigation.Screens
 import com.example.findme.viewmodels.ChatViewModel
 import io.getstream.chat.android.compose.ui.channels.ChannelsScreen
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
@@ -17,7 +20,7 @@ import io.getstream.chat.android.models.InitializationState
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ChatScreen() {
+fun ChatScreen(navController: NavController) {
 
     val viewModel: ChatViewModel = viewModel()
     val clientInitialisationState by viewModel.clientInitialisationState.collectAsState(initial = InitializationState.NOT_INITIALIZED)
@@ -29,7 +32,10 @@ fun ChatScreen() {
                 ChannelsScreen(
                     title = stringResource(id = R.string.app_name),
                     isShowingSearch = true,
-                    onItemClick = {},
+                    onItemClick = {
+                        val c = it.cid
+                        navController.navigate("${Screens.ChatMessagesScreen.name}/$c")
+                    },
                     onBackPressed = {}
                 )
                 // You can navigate to a specific channel or display a list of channels
