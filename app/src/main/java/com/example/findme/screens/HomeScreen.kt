c7a9ab8057e9c96c53ed4db73d39d3b7de40d2f8
Yeldos <eldos.amire@gmail.com>
2024-05-08 15:11:35 +0500

    Implemented horizontal pager in HomeScreen

--- app/src/main/java/com/example/findme/screens/HomeScreen.kt
+++ app/src/main/java/com/example/findme/screens/HomeScreen.kt
@@ -1,7 +1,10 @@
 package com.example.findme.screens
 
+import android.widget.Toast
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
@@ -23,80 +28,63 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.size.Size
 import com.example.findme.R
 
 @Composable
 fun HomeScreen() {
     Box(
-        modifier = Modifier
-            .fillMaxWidth()
-            .background(Color.LightGray)
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
     ) {
-        Image(
-            painter = painterResource(id = R.drawable.img_1),
-            contentDescription = "User Background",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Crop
-        )
-        Image(
-            painter = painterResource(id = R.drawable.img_2),
-            contentDescription = "User Photo",
-            modifier = Modifier
-                .size(200.dp)
-                .align(Alignment.Center)
-                .clip(CircleShape)
-        )
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(16.dp),
-            horizontalAlignment = Alignment.Start,
-            verticalArrangement = Arrangement.Bottom
-        ) {
+        EndlessHorizontalPager(
+            items = listOf("A", "B", "C"),
+            itemClicked = {
+                /*when (clickedItem) {
+                    "A" -> navController.navigate("screenA")
+                    "B" -> navController.navigate("screenB")
+                    "C" -> navController.navigate("screenC")
+                }*/
+            })
+    }
+}
 
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun EndlessHorizontalPager(
+    items: List<String>,
+    itemClicked: (String) -> Unit
+) {
+    val pageCount = Int.MAX_VALUE
+    val pagerState = rememberPagerState(
+        initialPage = pageCount / 2,
+        pageCount = { pageCount },
+    )
+
+    HorizontalPager(
+        modifier = Modifier
+            .background(Color(0xff469597))
+            .fillMaxSize()
+            .clickable {
+                val clickedItem = items[pagerState.settledPage % items.size]
+                itemClicked(clickedItem)
+            },
+        state = pagerState
+    ) { page ->
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
             Text(
-                text = "",
-                fontWeight = FontWeight.Bold,
-                fontSize = 20.sp,
-                modifier = Modifier
-                    .padding(16.dp, 0.dp, 8.dp, 4.dp),
-                style = MaterialTheme.typography.bodyMedium
+                text = items[page % items.size],
+                fontSize = 100.sp
             )
-            Row(
-                modifier = Modifier.padding(start = 16.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.LocationOn,
-                    contentDescription = "location",
-                    modifier = Modifier.size(18.dp)
-                )
-                Text(
-                    text = "user.location",
-                    modifier = Modifier
-                        .padding(4.dp, 0.dp, 0.dp, 0.dp),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
-            Row(
-                modifier = Modifier.padding(start = 16.dp, top = 4.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Default.Info,
-                    contentDescription = "gender",
-                    modifier = Modifier.size(18.dp)
-                )
-                Text(
-                    text = "user.gender",
-                    modifier = Modifier
-                        .padding(4.dp, 0.dp, 0.dp, 16.dp),
-                    style = MaterialTheme.typography.bodyMedium
-                )
-            }
         }
     }
 }
