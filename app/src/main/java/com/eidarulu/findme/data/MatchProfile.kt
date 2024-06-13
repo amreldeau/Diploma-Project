0402b67e03733ddce003cf04a09277ce5a897786
Yeldos <eldos.amire@gmail.com>
2024-06-13 11:12:10 +0500

    User profiles changed

--- app/src/main/java/com/eidarulu/findme/data/MatchProfile.kt
+++ app/src/main/java/com/eidarulu/findme/data/MatchProfile.kt
@@ -9,18 +9,13 @@ data class MatchProfile(
 )
 
 val profiles = listOf(
-    MatchProfile("Erlich Bachman", R.drawable.erlich),
-    MatchProfile("Richard Hendricks", R.drawable.richard),
-    MatchProfile("Laurie Bream", R.drawable.laurie),
-    MatchProfile("Russ Hanneman", R.drawable.russ),
-    MatchProfile("Dinesh Chugtai", R.drawable.dinesh),
-    MatchProfile("Monica Hall", R.drawable.monica),
-    MatchProfile("Bertram Gilfoyle", R.drawable.gilfoyle),
-
-    MatchProfile("Peter Gregory", R.drawable.peter),
-    MatchProfile("Jared Dunn", R.drawable.jared),
-    MatchProfile("Nelson Bighetti", R.drawable.big_head),
-    MatchProfile("Gavin Belson", R.drawable.gavin),
-    MatchProfile("Jian Yang", R.drawable.jian),
-    MatchProfile("Jack Barker", R.drawable.barker),
+    MatchProfile("Andrew Jenkins, 27", R.drawable.andrew_jenkins),
+    MatchProfile("Abigail Simmons, 23", R.drawable.abigail_simmons),
+    MatchProfile("Joshua Perry, 31", R.drawable.joshua_perry),
+    MatchProfile("Lily Anderson, 26", R.drawable.lily_anderson),
+    MatchProfile("Logan Russell, 26", R.drawable.logan_russell),
+    MatchProfile("Hannah Murphy, 27", R.drawable.hannah_murphy),
+    MatchProfile("Alexandra, 22", R.drawable.alexandra),
+    MatchProfile("Jacob Foster, 30", R.drawable.jacob_foster),
+    MatchProfile("Jackson Carter, 25", R.drawable.jackson_carter),
 )
